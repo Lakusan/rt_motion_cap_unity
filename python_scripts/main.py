64d2fd67f9e63480b754214a4103822e6ae8ce8d
Lakusan <Andreas.Lakus@googlemail.com>
2023-07-12 17:51:21 +0200

    main.py find cams and validate

--- python_scripts/main.py
+++ python_scripts/main.py
@@ -1,6 +1,20 @@
 from sys import exit
+from settings import Settings
+from cam_manager import CameraManager
 
 
 if __name__ == "__main__":
-    print("WORKS")
-    # exit()
+    print("Settings")
+    settings = Settings()
+    print("Camera Manager")
+    cam_manager = CameraManager()
+    print("find cams")
+    cam_manager.find_available_cameras()
+    if settings.available_cams_index != None:
+        print("WORKS")
+        print(f"len cams: {len(settings.available_cams_index)}")
+        print(settings.available_cams_index)
+    else:
+        print("cams = NONE")
+        print(f"len cams: {len(settings.available_cams_index)}")
+    exit()
