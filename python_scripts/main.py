7000e57f5a5799a1efd1aba42a978d359e2cb454
Lakusan <Andreas.Lakus@googlemail.com>
2023-07-13 19:36:00 +0200

    main.py rework workslow

--- python_scripts/main.py
+++ python_scripts/main.py
@@ -1,20 +1,29 @@
 from sys import exit
 from settings import Settings
 from cam_manager import CameraManager
+from cam_capturerer import CaptureThread
+import time
+import cv2
 
 
 if __name__ == "__main__":
-    print("Settings")
+    print("Load Settings")
     settings = Settings()
-    print("Camera Manager")
+    print("---> Settings Loaded")
+
+    print("Init Camera Manager")
     cam_manager = CameraManager()
+    print("---> Camera Manager initialized")
+
     print("find cams")
     cam_manager.find_available_cameras()
     if settings.available_cams_index != None:
-        print("WORKS")
         print(f"len cams: {len(settings.available_cams_index)}")
         print(settings.available_cams_index)
     else:
         print("cams = NONE")
         print(f"len cams: {len(settings.available_cams_index)}")
-    exit()
+    print("Init Capturing")
+    print("---> Capturing init")
+    capture = CaptureThread()
+    capture.start()
